7fde7cf615590ff03b2602141a19c00a56e4ded1
paul <paul@192.168.0.80>
2018-11-13 23:12:19 +0100

    rr

--- test/RechnerTest.java
+++ test/RechnerTest.java
@@ -4,23 +4,21 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import rechner.Rechner;
-
 public class RechnerTest {
-	public Rechner hi = new Rechner();
+	
 	@Test
 	public void testrechnen(){
-		int sol = addiere(1,3);
-		assertequals(4, sol);
+		Rechner hi = new Rechner();
+		assertequals("Test", 4, hi.addiere(1,3));
 	}
 	@Test
 	public void testrechnerr(){
-		int sol = faku(5);
-		assertequals(120, sol);
+		Rechner hi = new Rechner();
+		assertequals("Test", 120, hi.faku(5));
 	}
 	@Test
 	public void testrechne(){
-		int sol = sub(1,3);
-		assertequals(-2, sol);
+		Rechner hi = new Rechner();
+		assertequals("Test", -2, hi.sub(1,3));
 	}
 }
