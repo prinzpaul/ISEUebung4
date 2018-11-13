ba1c25955787015d50e0a31828a46cfde2cfbd9d
paul <paul@192.168.0.80>
2018-11-13 23:05:31 +0100

    r

--- test/RechnerTest.java
+++ test/RechnerTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import uk.co.placona.helloWorld.HelloWorld;
+import rechner.Rechner;
 
 public class RechnerTest {
 	public Rechner hi = new Rechner();
