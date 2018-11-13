bd172fe0517467940a008b2c4a7e6efd5d6141dd
paul <paul@192.168.0.80>
2018-11-13 23:01:48 +0100

    test

--- test/RechnerTest.java
+++ test/RechnerTest.java
@@ -7,22 +7,19 @@ import org.junit.Test;
 import uk.co.placona.helloWorld.HelloWorld;
 
 public class RechnerTest {
-	public Rechner hi;
+	public Rechner hi = new Rechner();
 	@Test
 	public void testrechnen(){
-		hi = new Rechner();
 		int sol = addiere(1,3);
 		assertequals(4, sol);
 	}
 	@Test
 	public void testrechnerr(){
-		hi = new Rechner();
 		int sol = faku(5);
 		assertequals(120, sol);
 	}
 	@Test
 	public void testrechne(){
-		hi = new Rechner();
 		int sol = sub(1,3);
 		assertequals(-2, sol);
 	}
